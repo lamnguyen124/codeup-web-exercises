3da3c366153033e23e410228104f8cd2cf5d4209
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-16 14:52:55 -0500

    es6

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,4 @@
+
 // $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey +  "/29.4241,-98.4936").done(function(data) {
 //     console.log(data);
 //     var dateObject = new Date(data.currently.time * 1000);
@@ -31,7 +32,7 @@ function buildHTML(weathers) {
                 icon = ele.src;
             } else if (weathers[2].icon === ele.icon) {
                 icon = ele.src;
-        }})
+            }})
     }
     weatherIcons();
     var weathersHTML = "";
@@ -68,62 +69,38 @@ var imageArray = [
         "src": "img/SVG/clear-night.svg",
         "icon": "clear-night"
     },
-     {
+    {
         "src": "img/SVG/clear-day.svg",
-         "icon": "clear-day"
+        "icon": "clear-day"
     },
     {
         "src": "img/SVG/snow.svg",
-         "icon": "snow"
+        "icon": "snow"
     },
-     {
+    {
         "src": "img/SVG/sleet.svg",
-         "icon": "sleet"
+        "icon": "sleet"
     },
-     {
+    {
         "src": "img/SVG/fog.svg",
-         "icon": "fog"
+        "icon": "fog"
     },
     {
         "src": "img/SVG/partly-cloud-day.svg",
-         "icon": "partly-cloudy-day"
+        "icon": "partly-cloudy-day"
 
     },
     {
         "src": "img/SVG/clear-night.svg",
-         "icon": "clear-night"
+        "icon": "clear-night"
     },
     {
         "src": "img/SVG/partly-cloud-day.svg",
-         "icon": "partly-cloudy-night"
+        "icon": "partly-cloudy-night"
     }
 ];
 var accessToken = mapboxToken;
 mapboxgl.accessToken = accessToken;
-
-// var sanAntonio;
-// var mapOptions = {
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/outdoors-v10',
-//     zoom: 15,
-//     center: [-98.489313, 29.426409]
-// };
-// var coordinates = document.getElementById('coordinates');
-// var map = new mapboxgl.Map(mapOptions);
-//
-// var marker = new mapboxgl.Marker({
-//     draggable: true
-// })
-//     .setLngLat([-98.489313, 29.426409])
-//     .addTo(map);
-//
-// function onDragEnd() {
-//     var lngLat = marker.getLngLat();
-//     coordinates.style.display = 'block';
-//     coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-// }
-//
-// marker.on('dragend', onDragEnd);
 
 var coordinates = document.getElementById('coordinates');
 var map = new mapboxgl.Map({
@@ -131,26 +108,6 @@ var map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-98.489313, 29.426409],
     zoom: 8
-});
-map.on('load', function () {
-    map.addSource('aerisweather-radar', {
-        "type": 'raster',
-        "tiles": [
-            'https://maps1.aerisapi.com/[clientId]_[clientKey]/radar/{z}/{x}/{y}/current.png',
-            'https://maps2.aerisapi.com/[clientId]_[clientKey]/radar/{z}/{x}/{y}/current.png',
-            'https://maps3.aerisapi.com/[clientId]_[clientKey]/radar/{z}/{x}/{y}/current.png',
-            'https://maps4.aerisapi.com/[clientId]_[clientKey]/radar/{z}/{x}/{y}/current.png'
-        ],
-        "tileSize": 256,
-        "attribution": "<a href='https://www.aerisweather.com/'>AerisWeather</a>"
-    });
-    map.addLayer({
-        "id": "radar-tiles",
-        "type": "raster",
-        "source": "aerisweather-radar",
-        "minzoom": 0,
-        "maxzoom":22
-    });
 });
 
 var marker = new mapboxgl.Marker({
@@ -171,51 +128,21 @@ function onDragEnd() {
 
 marker.on('dragend', onDragEnd);
 
-var geocoder = new MapboxGeocoder({ // Initialize the geocoder
-    accessToken: mapboxgl.accessToken, // Set the access token
-    mapboxgl: mapboxgl, // Set the mapbox-gl instance
-    marker: false // Do not use the default marker style
-});
+// var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+//     accessToken: mapboxgl.accessToken, // Set the access token
+//     mapboxgl: mapboxgl, // Set the mapbox-gl instance
+//     marker: false // Do not use the default marker style
+// });
+//
+// geocoder.on('result', function(ev) {// change the result to san antonio texas to hard code in start point// re do the geocoder.on to repopulate searched results
+//     map.getSource('single-point').setData(ev.result.geometry);
+//     var geoLat =  ev.result.geometry.coordinates[1];
+//     var geoLong = ev.result.geometry.coordinates[0];
+//     console.log(geoLat, geoLong);
+//
+//     var marker = new mapboxgl.Marker() // Initialize a new marker
+//         .setLngLat([geoLong, geoLat]) // Marker [lng, lat] coordinates
+//         .addTo(map); // Add the marker to the map
 
 map.addControl(geocoder);
-
-// // After the map style has loaded on the page,
-// // add a source layer and default styling for a single point
-// map.on('load', function() {
-//     map.addSource('single-point', {
-//         type: 'geojson',
-//         data: {
-//             type: 'FeatureCollection',
-//             features: []
-//         }
-//     });
-//
-//     map.addLayer({
-//         id: 'point',
-//         source: 'single-point',
-//         type: 'circle',
-//         paint: {
-//             'circle-radius': 10,
-//             'circle-color': '#448ee4'
-//         }
-//     });
-//
-//     // Listen for the `result` event from the Geocoder
-//     // `result` event is triggered when a user makes a selection
-//     //  Add a marker at the result's coordinates
-//     geoccoder.on('result', function(e) {
-//         map.getSource('single-point').setData(e.result.geometry);
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
 
