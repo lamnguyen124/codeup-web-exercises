de5e5fc694dc20920d06498e8e7065ebdea37124
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-18 15:06:00 -0500

    promises

--- js/promises.js
+++ js/promises.js
@@ -1,16 +1,31 @@
-const wait = new Promise((resolve, reject) => {
-    if (Math.random() > 0.5) {
-        resolve();
-    } else {
-        reject();
+const wait = (ms) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, 1000);
+    });
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+
+
+const gitName = prompt("what is your username?");
+fetch('https://api.github.com/users/${gitName}', {headers: {'Authorization': 'token ' + githubKey}})
+    .then(response => {
+        console.log(response);
+        return response.json()
+    })
+    .then(jsonified => console.log(jsonified))
+.then(usersArray => {
+    for (let user of usersArray) {
+        console.log(user.html_url);
     }
 });
 
-function username(){
-   const name = prompt("what is your username?");
-
+// function loadGithubUser(name) {
+//     return fetch(`https://api.github.com/users/${gitName}`)
+//         .then(response => response.json());
+//         console.log(response);
 }
-
-fetch(url, {headers: {'Authorization': 'githubKey'}})
-
-
+// fetch('https://api.github.com/users/users/:${gitName}/events')
